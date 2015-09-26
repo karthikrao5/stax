83292f8ef7b1947f3764ce1772decc3774a5c5b7
Karthik Rao <karthik.rao543@gmail.com>
2015-09-26 14:38:43 -0400

    working html

--- stax.js
+++ stax.js
@@ -1,43 +1,28 @@
-/*
-  Directive by HTML5 UP
-  html5up.net | @n33co
-  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
-(function($) {
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
 
-  skel.breakpoints({
-    wide: '(max-width: 1680px)',
-    normal: '(max-width: 1280px)',
-    narrow: '(max-width: 980px)',
-    narrower: '(max-width: 840px)',
-    mobile: '(max-width: 736px)',
-    mobilep: '(max-width: 480px)'
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
   });
 
-  $(function() {
-
-    var $window = $(window),
-      $body = $('body');
-
-    // Disable animations/transitions until the page has loaded.
-      $body.addClass('is-loading');
-
-      $window.on('load', function() {
-        $body.removeClass('is-loading');
-      });
-
-    // Fix: Placeholder polyfill.
-      $('form').placeholder();
-
-    // Prioritize "important" elements on narrower.
-      skel.on('+narrower -narrower', function() {
-        $.prioritize(
-          '.important\\28 narrower\\29',
-          skel.breakpoint('narrower').active
-        );
-      });
-
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
   });
 
-})(jQuery);
+  Template.timeline.helpers({
+      name: "nigga"
+  })
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
